388755345f583e6f7272f1546eb1faf10690cf68
febo <febo@anza.xyz>
2026-06-21 00:03:34 +0100

    Add account resize program

---
+++ benchmark/tests/account_resize.rs
@@ -0,0 +1,70 @@
+mod setup;
+
+use {
+    crate::setup::{run, setup, BASE_LAMPORTS, PROGRAM_ID},
+    mollusk_svm::result::Check,
+    solana_account::Account,
+    solana_address::Address,
+    solana_instruction::{error::InstructionError, AccountMeta, Instruction},
+};
+
+fn instruction(
+    program_id: &Address,
+    new_len: usize,
+    is_writable: bool,
+) -> (Instruction, Vec<(Address, Account)>) {
+    let account = Address::new_unique();
+
+    let accounts = vec![(account, Account::new(BASE_LAMPORTS, 0, program_id))];
+    let account_metas = vec![AccountMeta {
+        pubkey: account,
+        is_signer: false,
+        is_writable,
+    }];
+
+    (
+        Instruction {
+            program_id: *program_id,
+            accounts: account_metas,
+            data: new_len.to_le_bytes().to_vec(),
+        },
+        accounts,
+    )
+}
+
+#[test]
+fn test_account_resize() {
+    let mollusk = setup(&PROGRAM_ID, "account_resize");
+    let (instruction, accounts) = instruction(&PROGRAM_ID, 72, true);
+
+    let (key, result) = run(&mollusk, &instruction, &accounts, &[Check::success()]);
+
+    let account = result.get_account(&key);
+    assert!(account.is_some());
+
+    // The account data length should have increased.
+
+    assert_eq!(account.unwrap().data.len(), 72);
+}
+
+#[test]
+fn fail_account_resize_with_readonly_account() {
+    let mollusk = setup(&PROGRAM_ID, "account_resize");
+    let (instruction, accounts) = instruction(&PROGRAM_ID, 72, false);
+
+    let (key, result) = run(
+        &mollusk,
+        &instruction,
+        &accounts,
+        &[Check::instruction_err(
+            InstructionError::ReadonlyDataModified,
+        )],
+    );
+
+    let account = result.get_account(&key);
+    assert!(account.is_some());
+
+    // The account data length should not have changed.
+
+    assert_eq!(account.unwrap().data.len(), 0);
+}
---
+++ programs/account-resize/src/lib.rs
@@ -0,0 +1,34 @@
+use abiv2::{
+    account::Account, context::InstructionContext, error::ProgramError, program_entrypoint,
+    ProgramResult,
+};
+
+program_entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    _context: &InstructionContext,
+    accounts: &[Account],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let [account, ..] = accounts else {
+        return Err(ProgramError::NotEnoughAccountKeys);
+    };
+
+    let new_len = if instruction_data.len() == 8 {
+        u64::from_le_bytes(unsafe { *(instruction_data.as_ptr() as *const [u8; 8]) })
+    } else {
+        return Err(ProgramError::InvalidInstructionData);
+    };
+
+    if account.data_len() != 0 {
+        return Err(ProgramError::InvalidAccountData);
+    }
+
+    account.resize(new_len as usize);
+
+    if account.data_len() == 0 {
+        return Err(ProgramError::AccountDataTooSmall);
+    }
+
+    Ok(())
+}
