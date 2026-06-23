9192aa4ed65da27bf357f8996b523c415e71f64e
febo <febo@anza.xyz>
2026-06-23 14:08:17 +0100

    Add max_account_data_len test

--- benchmark/tests/account_resize.rs
+++ benchmark/tests/account_resize.rs
@@ -68,3 +68,23 @@ fn fail_account_resize_with_readonly_account() {
 
     assert_eq!(account.unwrap().data.len(), 0);
 }
+
+#[test]
+fn test_account_resize_max_data_len() {
+    // Maximum account data length.
+    //
+    // https://github.com/anza-xyz/agave/blob/master/transaction-context/src/lib.rs#L19
+    const MAX_ACCOUNT_DATA_LEN: usize = 10 * 1024 * 1024;
+
+    let mollusk = setup(&PROGRAM_ID, "account_resize");
+    let (instruction, accounts) = instruction(&PROGRAM_ID, MAX_ACCOUNT_DATA_LEN, true);
+
+    let (key, result) = run(&mollusk, &instruction, &accounts, &[Check::success()]);
+
+    let account = result.get_account(&key);
+    assert!(account.is_some());
+
+    // The account data length should have increased.
+
+    assert_eq!(account.unwrap().data.len(), MAX_ACCOUNT_DATA_LEN);
+}
